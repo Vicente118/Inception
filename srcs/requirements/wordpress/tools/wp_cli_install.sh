23613b1f74ac982f73391604497bd84ad2e76f6a
Vicente118 <vdarras27@gmail.com>
2025-02-05 19:35:41 +0100

    Final push

--- srcs/requirements/wordpress/tools/wp_cli_install.sh
+++ srcs/requirements/wordpress/tools/wp_cli_install.sh
@@ -4,13 +4,21 @@ sleep 10
 
 if [ ! -f "/var/www/html/wp-config.php" ]
 then
-	wp core download --path="/var/www/html" --allow-root
-	wp config create --path="/var/www/html" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
-	wp core install --path="/var/www/html" --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
-	wp user create --path="/var/www/html" --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASS --role='contributor'
+    echo "Téléchargement de WordPress..."
+    wp core download --path="/var/www/html" --allow-root
+    
+    echo "Création du fichier wp-config.php..."
+    wp config create --path="/var/www/html" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --skip-check
+    
+    echo "Installation de WordPress..."
+    wp core install --path="/var/www/html" --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+    
+    echo "Création de l'utilisateur supplémentaire..."
+    wp user create "$WP_USER" "$WP_EMAIL" --role=subscriber --user_pass="$WP_PASS" --allow-root --path=/var/www/html
+    
 else
-	echo "nopp"
-
+    echo "WordPress est déjà installé."
 fi
 
 exec /usr/sbin/php-fpm7.4 -F
+
